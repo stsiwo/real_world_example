fce61535a9a9b00b82eb44760250f3b57d16b8be
Satoshi Iwao <stsiwo@gmail.com>
2018-10-02 12:39:14 -0700

    after ejection and fix eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,46 @@
 module.exports = {
-    "env": {
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
+  "env": {
+      "es6": true,
+      "browser": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      "indent": [
+          "warn",
+          2
+      ],
+      "quotes": [
+          "warn",
+          "double"
+      ],
+      "semi": [
+          "warn",
+          "never"
+      ], 
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "no-console": [
+        "error", 
+        { 
+          "allow": [
+            "log",
             "warn",
-            2
-        ],
-        "quotes": [
-            "warn",
-            "double"
-        ],
-        "semi": [
-            "warn",
-            "never"
-        ], 
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-    }
+            "error" 
+          ] 
+        }
+      ], 
+      "no-undef": "warn",
+      "no-useless-escape": "warn",
+      "no-unused-vars": "warn"
+  }
 };
